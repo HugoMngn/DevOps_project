0656a6b074c3fd5b452dbd1fcb3d20d46b01cb4d
Drac0fire <166018262+HugoMngn@users.noreply.github.com>
2024-12-17 14:25:22 +0100

    test fonctionnel, verifie bcq et sont généré

--- src/app/components/title/title.component.spec.ts
+++ src/app/components/title/title.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { TitleComponent } from './title.component';
 
 describe('TitleComponent', () => {
@@ -7,7 +8,7 @@ describe('TitleComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TitleComponent ]
+      imports: [TitleComponent]
     })
     .compileComponents();
 
